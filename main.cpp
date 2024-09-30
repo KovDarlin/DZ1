5b3595863734253b662cda38ba46f2dbf6a24bcb
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2024-09-30 20:57:28 +0300

    ex 4 was done

--- main.cpp
+++ main.cpp
@@ -2,5 +2,46 @@
 using namespace std;
 
 int main(){
+	int x;
+	cout << "Enter the first number: ";
+	cin >> x;
+	char op;
+	cout << "Enter an operation +, -, *, /: ";
+	cin >> op;
+	int y;
+	cout << "Enter the second number: ";
+	cin >> y;
+	int res;
 
+	int* x_p = &x;
+	int* y_p = &y;
+	int* res_p = &res;
+
+	if (op == '+') {
+		*res_p = *x_p + *y_p;
+	}
+	else if (op == '-') {
+		*res_p = *x_p - *y_p;
+	}
+	else if (op == '*') {
+		if (*x_p != 0 && *y_p != 0) {
+			*res_p = *x_p * *y_p;
+		}
+		else {
+			*res_p = 0;
+		}
+	}
+	else if (op == '/') {
+		if (*x_p != 0 && *y_p != 0) {
+			*res_p = *x_p / *y_p;
+		}
+		else {
+			cout << "Cannot be divided by zero!" << endl;
+		}
+	}
+	else {
+		cout << "Invalid operator!" << endl;
+	}
+
+	cout << "Result: " << *res_p << endl;
 }
