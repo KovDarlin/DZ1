22f9e9d0545f7677db0f9f2224029ff900d410a0
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2024-09-30 20:54:32 +0300

    Ex 2 was done

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main(){
+	int x;
+	cout << "Enter the number: ";
+	cin >> x;
 
+	int* x_p = &x;
+
+	if (*x_p > 0) {
+		cout << *x_p << " the number is positive " << endl;
+	}
+	else if (*x_p < 0) {
+		cout << *x_p << " the number is negative " << endl;
+	}
+	else {
+		cout << *x_p << " the number is zero " << endl;
+	}
 }
