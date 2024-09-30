f2adf78af6f76fa9e43c34d6aff6fe8b95a341d4
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2024-09-30 20:59:11 +0300

    ex 5 was done

--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main(){
+	const int elements = 5;
+	int masiv[elements] = { 10, 4, 26, 2, 18 };
 
+	int* masiv_p = masiv;
+	int sum = 0;
+
+
+	for (int i = 0; i < elements; i++) {
+		sum += *masiv_p;
+		masiv_p++;
+	}
+
+	cout << "The sum of array elements: " << sum << endl;
 }
