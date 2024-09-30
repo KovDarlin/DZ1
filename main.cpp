3b7326dba674a5612e953534369affb060de4bcf
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2024-09-30 20:51:34 +0300

    Ex 1 from hm was done

--- main.cpp
+++ main.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main(){
+	int x;
+	cout << "Enter the first number: ";
+	cin >> x;
+	int y;
+	cout << "Enter the second number: ";
+	cin >> y;
+
+	int* x_p = &x;
+	int* y_p = &y;
+
+	if (*x_p > *y_p) {
+		cout << *x_p << " more and " << *y_p << " less." << endl;
+	}
+	else {
+		cout << *y_p << " more and " << *x_p << " less." << endl;
+	}
 
 }
