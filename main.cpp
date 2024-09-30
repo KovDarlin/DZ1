3df59fc11db7310b5c9fe2ab7d949f3efaaa9341
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2024-09-30 20:55:57 +0300

    ex3 was done

--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main(){
+	int x = 99;
+	int y = 13;
+
+
+	int* x_p = &x;
+	int* y_p = &y;
+	int temp = *x_p;
+
+	*x_p = *y_p;
+	*y_p = temp;
+
+	cout << "The numbers are reversed: " << x << " " << y << endl;
 
 }
